a4c32d91589a622d924235ea361b1444e6e8e11c
Nizomiddin Rakhimberdiev <75323193+nizomiddin-rakhimberdiev@users.noreply.github.com>
2022-04-29 22:58:35 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -95,7 +95,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db_url("DATABASE_URL")
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
